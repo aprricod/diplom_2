846ff5a4733c4074f19989ab3e6dcfc8aa1d42b7
Artem Borisenko <aprricod@gmail.com>
2022-10-16 19:46:32 +0200

    rest client added

---
+++ src/main/java/ru/yandex/praktikum/StellarRestClient.java
@@ -0,0 +1,16 @@
+package ru.yandex.praktikum;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+public class StellarRestClient {
+    private final String BASE_URL = "https://stellarburgers.nomoreparties.site/";
+
+    protected RequestSpecification baseSpec(){
+        return new RequestSpecBuilder()
+                .setContentType(ContentType.JSON)
+                .setBaseUri(BASE_URL)
+                .build();
+    }
+}
