f540ab989985779e69a07fea4b04b9c4bf523e1f
Artem Borisenko <aprricod@gmail.com>
2022-10-23 00:24:23 +0200

    successful change user test added

--- src/test/java/tests/ChangeUserDataTest.java
+++ src/test/java/tests/ChangeUserDataTest.java
@@ -1,8 +1,9 @@
 package tests;
 
-import io.restassured.RestAssured;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+import io.qameta.allure.junit4.DisplayName;
 import io.restassured.http.ContentType;
-import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
@@ -23,39 +24,41 @@ public class ChangeUserDataTest {
     DeleteUser deleteUser = new DeleteUser();
     AuthUser authUser = new AuthUser();
 
-    String userName = RandomStringUtils.randomAlphabetic(6);
-    String userEmail = RandomStringUtils.randomAlphanumeric(10) + "@mail.com";
-    String userPassword = RandomStringUtils.randomAlphabetic(6);
+    String userName;
+    String userEmail;
+    String userPassword;
     String tokenFull;
     String tokenValue;
 
-    //    String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNGRiYjYxYzE2MWRhMDAxYjc3YmE3MyIsImlhdCI6MTY2NjM4NjIyNiwiZXhwIjoxNjY2Mzg3NDI2fQ.gMQqf61zOvM0qM8IgxM1g_egkIzElUUWlE4hE3O1L7Y";
-    String requestBody = "{\"email\" : \"test99999@yopmail.com\", \"name\" : \"restname\"}";
+    String requestBody = "{\"email\" : \"patch999@yopmail.com\", \"name\" : \"PatchName\"}";
 
     String log = "test999@yopmail.com";
     String pass = "123456";
 
     @Before
-//    public void createNewUser() {
-//        ValidatableResponse create = createUser.postFullUserData(userName, userEmail, userPassword);
-//        tokenFull = create.extract().body().path("accessToken");
-//        tokenValue = tokenFull.substring(7);
-//
-//        create.assertThat()
-//                .statusCode(200)
-//                .and()
-//                .body("success", equalTo(true));
-//    }
-
-    public void successfulLoginTest() {
-        ValidatableResponse auth = authUser.authUserWithFullData(log, pass);
-        tokenFull = auth.extract().body().path("accessToken");
+    @Step("before")
+    public void createNewUser() {
+        userName = RandomStringUtils.randomAlphabetic(6);
+        userEmail = RandomStringUtils.randomAlphanumeric(10) + "@mail.com";
+        userPassword = RandomStringUtils.randomAlphabetic(6);
+        ValidatableResponse create = createUser.postFullUserData(userName, userEmail, userPassword);
+        tokenFull = create.extract().body().path("accessToken");
         tokenValue = tokenFull.substring(7);
-        auth.assertThat()
+        create.assertThat()
                 .statusCode(200)
                 .and()
                 .body("success", equalTo(true));
     }
+
+//    public void successfulLoginTest() {
+//        ValidatableResponse auth = authUser.authUserWithFullData(log, pass);
+//        tokenFull = auth.extract().body().path("accessToken");
+//        tokenValue = tokenFull.substring(7);
+//        auth.assertThat()
+//                .statusCode(200)
+//                .and()
+//                .body("success", equalTo(true));
+//    }
 
 //    @Test
 //    public void getUserDataTest() {
@@ -69,31 +72,45 @@ public class ChangeUserDataTest {
 //    }
 
     @Test
-    public void patchUserDataTest() {
-        Response response = given()
-                .log().all()
+    @DisplayName("Change user")
+    @Description("Successful user data changing with authentication")
+    @Step("Change user")
+    public void patchUserDataWithAuthTest() {
+        ValidatableResponse response = given()
                 .header("authorization", "bearer " + tokenValue)
                 .contentType(ContentType.JSON)
                 .and()
                 .body(requestBody)
                 .when()
                 .patch("https://stellarburgers.nomoreparties.site/api/auth/user")
-                .then()
-                .log().all()
-                .extract().response();
+                .then();
 
+        String email = response.extract().body().path("user.email");
+        String name = response.extract().body().path("user.name");
+
+        response.assertThat()
+                .statusCode(200)
+                .and()
+                .body("success", equalTo(true))
+                .and()
+                .body("user.email", equalTo("patch999@yopmail.com"))
+                .body("user.name", equalTo("PatchName"));
+
+        System.out.println(userName);
+        System.out.println(userEmail);
+        System.out.println(userPassword);
         System.out.println(requestBody);
-
-        System.out.println(response.asString());
-
+        System.out.println(email);
+        System.out.println(name);
     }
 
-//    @After
-//    public void deleteUserTest() {
-//        ValidatableResponse delete = deleteUser.deleteUser(tokenValue);
-//        delete.assertThat()
-//                .statusCode(202)
-//                .and()
-//                .body("success", equalTo(true));
-//    }
+    @After
+    @Step("after")
+    public void deleteUserTest() {
+        ValidatableResponse delete = deleteUser.deleteUser(tokenValue);
+        delete.assertThat()
+                .statusCode(202)
+                .and()
+                .body("success", equalTo(true));
+    }
 }
