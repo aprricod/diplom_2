946ba5499e8bfaa67f9f712ae530c9d38e3c0269
Artem Borisenko <aprricod@gmail.com>
2022-10-27 20:17:56 +0200

    orders tests WIP

--- src/test/java/tests/CreateOrderTest.java
+++ src/test/java/tests/CreateOrderTest.java
@@ -1,27 +1,54 @@
 package tests;
 
 import io.restassured.response.ValidatableResponse;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 import ru.yandex.praktikum.CreateOrder;
+import ru.yandex.praktikum.CreateUser;
+import ru.yandex.praktikum.DeleteUser;
 
 import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.notNullValue;
 
 public class CreateOrderTest {
     CreateOrder createOrder = new CreateOrder();
+    CreateUser createUser = new CreateUser();
+    DeleteUser deleteUser = new DeleteUser();
+
+    String userName = RandomStringUtils.randomAlphabetic(6);
+    String userEmail = RandomStringUtils.randomAlphanumeric(10) + "@mail.com";
+    String userPassword = RandomStringUtils.randomAlphabetic(6);
+    String tokenFull;
 
     String validIngredients = "{\"ingredients\" : [\"61c0c5a71d1f82001bdaaa6d\", \"61c0c5a71d1f82001bdaaa72\"]}";
     String invalidIngredients = "{\"ingredients\" : [\"test1\", \"test2\"]}";
-    String token = "";
 
     // заказ с авторизацией, с ингредиентами
     @Test
     public void createOrderWithAuthWithIngredientsTest() {
-        ValidatableResponse placeOrder = createOrder.createOrderWithAuthWithIngredients(token, validIngredients);
+        ValidatableResponse create = createUser.postFullUserData(userName, userEmail, userPassword);
+        tokenFull = create.extract().body().path("accessToken");
+        String tokenValue = tokenFull.substring(7);
+
+        create.assertThat()
+                .statusCode(200)
+                .and()
+                .body("success", equalTo(true))
+                .and()
+                .body("accessToken", notNullValue());
+
+        ValidatableResponse placeOrder = createOrder.createOrderWithAuthWithIngredients(tokenValue, validIngredients);
         placeOrder.assertThat()
                 .log().all()
                 .statusCode(200)
                 .and()
                 .log().all()
+                .body("success", equalTo(true));
+
+        ValidatableResponse delete = deleteUser.deleteUser(tokenValue);
+        delete.assertThat()
+                .statusCode(202)
+                .and()
                 .body("success", equalTo(true));
     }
 
